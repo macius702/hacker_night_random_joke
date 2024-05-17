e51a1a0aceaeece0cc91cc3fb90494c71ee11633
Matiki <maciej.tomasz.krzeminski@gmail.com>
2024-05-17 21:42:56 +0200

    Change to #update

--- src/random_ICP_backend/src/lib.rs
+++ src/random_ICP_backend/src/lib.rs
@@ -1,31 +1,24 @@
+use ic_cdk::update;
 
 const JOKES: [&str; 10] = [
-    "Dlaczego komputerowi nie warto ufać? Bo kiedy mówi, że ma 4 bajty, to kłamie. Ma 1 bajt i 3 bity.",
-    "Dlaczego programista nie lubi natury? Bo ma za dużo bugów.",
     "Dlaczego programista zawsze wraca do domu o północy? Bo adres powrotny to 0x00.",
     "Jakie jest ulubione miejsce programistów na randki? GitHub.",
-    "Dlaczego programiści zawsze mieszają Halloween z Bożym Narodzeniem? Bo 31 OCT = 25 DEC.",
-    "Dlaczego programista nie używa łóżka? Bo nie może znaleźć śpiwora.",
-    "Dlaczego programista nie używa Facebooka? Bo nie może polubić strony, która nie ma źródła.",
-    "Dlaczego programista nie idzie do lekarza? Bo Google ma wszystkie odpowiedzi.",
-    "Dlaczego programista nie idzie na siłownię? Bo nie lubi ciężkich obciążeń.",
-    "Dlaczego programista nie idzie do kina? Bo nie lubi trailerów."
+    "Szef do pracownika: - W tym tygodniu to już czwarte spóźnienie. Jaki z tego należy wyciągnąć wniosek? - Że dziś czwartek.",
+    "Co mówi leniwa osoba, gdy słyszy coś niedorzecznego? Nie chce mi się w to wierzyć.",
+    "Dlaczego koza wydaje się tania w utrzymaniu? Bo może zeżreć cokolwiek. A dlaczego koza jest droga w utrzymaniu? Bo może zeżreć cokolwiek.",
+    "Jaka jest najbardziej królewska cześć trójkąta? Jego wysokość",
+    "Pamiętajcie, Titanica zbudowali zawodowcy, a Arkę Noego amatorzy.",
+    "Jakie kwiaty zabijają żony? Żonkile.",
+    "Dlaczego w kosmosie nie ma dobrych imprez? Bo nie ma atmosfery.",
+    "Jaka jest różnica między młotkiem a kukurydzą? Młotek się dłużej gotuje."
 ];
-#[ic_cdk::query]
-fn greet(name: String) -> String {
-    //let (bytes,): (Vec<u8>,) = ic_cdk::api::call::call(Principal::management_canister(), "raw_rand", ()).await.unwrap();
-    //let (r,) = ic_cdk::api::management_canister::main::raw_rand().await.unwrap();
-    //convert Vec><u8> into integer from 0 to 9 
-    let now = ic_cdk::api::time();
-    let byte = now % 10;
 
-
-    let index = (byte as usize) % JOKES.len();
-    let string = JOKES[index];
-    return string.to_string();
-
-
-    //format!("Hello, {}!", name)
+#[update]
+async fn random_joke() -> String {
+    let (r,) = ic_cdk::api::management_canister::main::raw_rand().await.unwrap();
+    let byte = r[0] as usize;
+    let index = byte % 10;
+    let joke = JOKES[index];
+    return joke.to_string();
 }
-
 
