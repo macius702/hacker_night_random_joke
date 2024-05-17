42cacb58bb3abe9360284e726f0cf9f765d2cc0c
Matiki <maciej.tomasz.krzeminski@gmail.com>
2024-05-17 11:26:21 +0200

    Commit on the end of Hacker Night

--- src/random_ICP_backend/src/lib.rs
+++ src/random_ICP_backend/src/lib.rs
@@ -1,4 +1,32 @@
+
+const STRINGS: [&str; 10] = [
+    "String1",
+     "String2",
+      "String3",
+       "String4",
+        "String5",
+         "String6", 
+         "String7",
+          "String8",
+           "String9",
+            "String10"
+            ];
+
 #[ic_cdk::query]
 fn greet(name: String) -> String {
-    format!("Hello, {}!", name)
+    //let (bytes,): (Vec<u8>,) = ic_cdk::api::call::call(Principal::management_canister(), "raw_rand", ()).await.unwrap();
+    //let (r,) = ic_cdk::api::management_canister::main::raw_rand().await.unwrap();
+    //convert Vec><u8> into integer from 0 to 9 
+    let now = ic_cdk::api::time();
+    let byte = now % 10;
+
+
+    let index = (byte as usize) % STRINGS.len();
+    let string = STRINGS[index];
+    return string.to_string();
+
+
+    //format!("Hello, {}!", name)
 }
+
+
